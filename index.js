dd0a9e0ba4d3f4dec98a9b27ce8cac1775b18fc0
Angela <balderasangela@gmail.com>
2022-11-09 21:42:34 -0500

    COMPLETED ASSIGNMENT

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record) {
+    let resultWin = record.find(element => element["result"] === "W");
+    if (resultWin === undefined) {
+        return undefined
+    } else {
+        return resultWin["year"];
+    }
+}
